19f3ca24fe4fda61f3bdc63fff11278c1b588e92
Masum Billal <2643770+proafxin@users.noreply.github.com>
2020-10-31 05:19:33 +0600

    removed manual joining and fixed common fields

--- flatten_json.py
+++ flatten_json.py
@@ -1,5 +1,6 @@
 from pandas import (
     json_normalize,
+    merge,
     DataFrame,
 )
 from numpy import (
@@ -14,28 +15,50 @@ def flatten(dictionary):
         else:
             result[key] = DataFrame([value], columns=[key])
         result[key] = result[key].replace(nan, '', regex=True)
-    parsed_result = []
+    # parsed_result = []
+    df = DataFrame()
     for key in result:
         #print(key)
         #print(result[key].to_numpy())
-        if len(parsed_result) < 1:
-            parsed_result = result[key].to_numpy()
+        if df.shape[0] < 1:
+            # parsed_result = result[key].to_numpy()
+            df = result[key]
         else:
-            result_left = parsed_result
+            # result_left = parsed_result
             
-            result_right = result[key].to_numpy()
-            res = []
+            # result_right = result[key].to_numpy()
+            # res = []
             #print(result_left, result_right)
-            for left_row in result_left:
-                for right_row in result_right:
-                    tmp = list(left_row)
-                    tmp.extend(list(right_row))
-                    res.append(tmp)
-            parsed_result = res
+            # for left_row in result_left:
+            #     for right_row in result_right:
+            #         tmp = list(left_row)
+            #         tmp.extend(list(right_row))
+            #         res.append(tmp)
+            # parsed_result = res
+            cols = set(df.columns.tolist())
+            cols = cols.intersection(result[key].columns.tolist())
+            cols = list(cols)
+            if len(cols) > 0:
+                df = merge(
+                    left=df,
+                    right=result[key],
+                    left_on=cols,
+                    right_on=cols,
+                )
+            else:
+                df['common'] = 1
+                result[key]['common'] = 1
+                df = merge(
+                    left=df,
+                    right=result[key],
+                    left_on=['common'],
+                    right_on=['common'],
+                )
+                df = df.drop('common', axis=1)
         #print(parsed_result)
         #print('')
-    columns = []
-    for key in result:
-        columns.extend(result[key].columns.tolist())
-    df = DataFrame(parsed_result, columns=columns)
+    # columns = []
+    # for key in result:
+        # columns.extend(result[key].columns.tolist())
+    # df2 = DataFrame(parsed_result,)
     return df
