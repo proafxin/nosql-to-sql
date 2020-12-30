dc51b0414a8a8be499f29fecd4ed451ed7fe2c91
Masum Billal <2643770+proafxin@users.noreply.github.com>
2020-12-30 13:48:49 +0600

    fixed pip installation entry point issue

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ README = f.read()
 
 setup(
     name='flatten',
-    version='1.1',
+    version='1.3',
     author='Masum Billal',
     author_email='billalmasum93@gmail.com',
     url='https://github.com/proafxin/nosql-to-sql',
